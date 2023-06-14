98258ede5e75931312fef04f38c8edf9c2223cf8
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-15 01:51:12 +0300

    Add custom methods in the models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts_counter + 1)
+  end
+
+  def recenet_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
