0564afe5db7eddacdbc3cdf88acd9b2bc7f48166
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-14 18:39:02 +0300

    Revert changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
-    has_many :likes
+    has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
 end
