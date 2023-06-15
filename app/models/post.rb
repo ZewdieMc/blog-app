d81a562d07fca5989b43c3f5e02c4dc3c1e3b069
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-15 17:18:24 +0300

    Add Post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   def update_posts_counter
     author.update(posts_counter: author.posts.count + 1)
   end
