9cf4e9b97c31777c18d793c936e860ebe84c9d85
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-15 18:49:32 +0300

    Add spec for user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  validates :title, presence: true
-  validates :title, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
   def update_posts_counter
-    author.update(posts_counter: author.posts.count + 1)
+    author.update(posts_counter: author.posts_counter.count + 1)
   end
 
   def recenet_comments
