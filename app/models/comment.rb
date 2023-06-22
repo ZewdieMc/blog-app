b602c7db717a4daa4572b0d0393bc436277595c9
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-22 15:55:47 +0300

    Upudate the counter methods in each model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   def update_comments_counter
-    post.update(comments_counter: post.comments_counter + 1)
+    post_comments = post.comments_counter || 0
+    post.update(comments_counter: post_comments + 1)
   end
 end
