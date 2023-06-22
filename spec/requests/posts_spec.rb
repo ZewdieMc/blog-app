eb04dc050ede7d2a92a9e88c63614d5fd603a669
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-22 08:42:25 +0300

    Wrirte test for route GET users/:id/posts

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+describe "GET /users/:id/posts" do
+  before(:all) do
+    @user = User.create(name: 'John Doe', posts_counter: 0)
+  end
+  it "returns 200 http status and success" do
+    get user_posts_path(@user)
+    expect(response).to have_http_status(200)
+    expect(response).to be_successful
+  end
+  it 'renders the index template' do
+    get user_posts_path(@user)
+    expect(response).to render_template(:index)
+  end
+  it 'the response body includes correct placeholder text.' do
+    get user_posts_path(@user)
+    expect(response.body).to include("Here is a list of posts")
+  end
+end
