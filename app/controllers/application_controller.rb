ff1f9da8774284490201ee8791f5a9ac5678d2cd
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-28 15:31:22 +0300

    Configure controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user ||= User.first
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  # This method is used to add additional parameters to the Devise authentication process.
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit 
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to root_path, notice: "User updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    
+  def user_params
+    params.require(:user).permit(:photo, :bio)
+  end
 end
